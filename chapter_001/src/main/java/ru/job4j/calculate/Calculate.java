a4af5b9f111a6275a746f7a8814d514f878cf3cc
MichaelDorogov <mailto:mimeddo@yandex.ru>
2018-08-31 10:55:06 +0300

    Исправление по code review, добавлена пропущенная папка java

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -4,6 +4,8 @@ package ru.job4j.calculate;
  * Calculate.
  *
  * @author Michael Dorogov (mimeddo@yandex.ru)
+ * @version $Id$
+ * @since 0.1
  */
 public class Calculate {
     /**
@@ -13,5 +15,15 @@ public class Calculate {
      */
     public static void main(String[] args) {
         System.out.println("Hello word");
+    }
+
+    /**
+     * Method echo.
+     *
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
