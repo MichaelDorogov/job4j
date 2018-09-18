2e89b48f65dd2626a06b8cb9561b80d4e26c18d0
MichaelDorogov <mailto:mimeddo@yandex.ru>
2018-09-18 17:50:33 +0300

    4.4. Максимум из трех чисел

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -18,4 +18,22 @@ public class MaxTest {
         Max max = new Max();
         Assert.assertThat(max.max(1, 2), is(2));
     }
+
+    @Test
+    public void whenFirstGreaterThenOther() {
+        Max max = new Max();
+        Assert.assertThat(max.max(7, 5, 3), is(7));
+    }
+
+    @Test
+    public void whenSecondGreaterThenOther() {
+        Max max = new Max();
+        Assert.assertThat(max.max(3, 7, 5), is(7));
+    }
+
+    @Test
+    public void whenThirdGreaterThenOther() {
+        Max max = new Max();
+        Assert.assertThat(max.max(3, 5, 7), is(7));
+    }
 }
