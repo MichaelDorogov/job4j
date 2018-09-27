8d9cdf841439837bb492f42f19177f756d93b7c7
MichaelDorogov <mailto:mimeddo@yandex.ru>
2018-09-27 10:56:43 +0300

    6.0. Убран Math.pow().

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class Square {
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
-        for (int i = 0; i < bound; ) {
-            rst[i] = (int) Math.pow(++i, 2d);
+        for (int i = 1; i <= bound; i++) {
+            rst[i - 1] = i * i;
         }
         return rst;
     }
