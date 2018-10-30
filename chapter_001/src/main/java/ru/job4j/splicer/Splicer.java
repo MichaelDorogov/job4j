5273158006fcbb75ebf99184ea1824cd6cc97fa0
MDorogov <MDorogov@egartech.com>
2018-10-30 15:50:58 +0300

    Часть 001. Базовый синтаксис / 7. Контрольные вопросы и тестовое задание./ Исправление замечаний по code review

--- chapter_001/src/main/java/ru/job4j/splicer/Splicer.java
+++ chapter_001/src/main/java/ru/job4j/splicer/Splicer.java
@@ -4,25 +4,14 @@ public class Splicer {
     public int[] splice(int[] arr1, int[] arr2) {
         int[] answer = new int[arr1.length + arr2.length];
         int m = 0, n = 0, k = 0;
-        while (m < arr1.length && n < arr2.length) {
-            if (arr1[m] < arr2[n]) {
-                answer[k] = arr1[m];
-                m++;
+        while (m + n < answer.length) {
+            if (m < arr1.length && n < arr2.length) {
+                answer[k++] = arr1[m] < arr2[n] ? arr1[m++] : arr2[n++];
+            } else if (m < arr1.length) {
+                answer[k++] = arr1[m++];
             } else {
-                answer[k] = arr2[n];
-                n++;
+                answer[k++] = arr2[n++];
             }
-            k++;
-        }
-        while (m < arr1.length) {
-            answer[k] = arr1[m];
-            m++;
-            k++;
-        }
-        while (n < arr2.length) {
-            answer[k] = arr2[n];
-            n++;
-            k++;
         }
         return answer;
     }
